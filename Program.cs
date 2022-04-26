d35072fae65f60c9a337832937beab52beaf31c5
Aaron Jackson <aaron.jackson@covan.com>
2022-04-26 11:10:47 -0500

    Made it a little funner i guess

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Number_Guessing_Game
 {
@@ -13,22 +14,31 @@ namespace Number_Guessing_Game
             bool win = false;
             string rndNumber = rndNum.ToString();
             int guessCount = 1;
+            List<string> guesses = new List<string>(); //list to hold all of the guesses
 
             while (win == false)
             {
-                Console.WriteLine("Guess the number between 1 and 26"); //changed string to reflect number change to 26
+                Console.Write("Guess a number between 1 and 26: "); //formatted the sentence a little differently
                 yourGuess = Console.ReadLine();
+                guesses.Add(yourGuess);                                 //add guess to list
 
                 if (yourGuess == rndNumber)
                 {
-                    Console.WriteLine($"Congrats you guessed the number in {guessCount} guess(es)!");
+                    Console.WriteLine($"\nCongrats you guessed the number in {guessCount} guess(es)!");
                     win = true;
                     Console.ReadKey();
                 }
 
                 else
                 {
-                    Console.WriteLine("Sorry, try again. Make sure you enter a number.");
+                    Console.WriteLine("\n -- Sorry, try again. Make sure you enter a number. --\n");   //Added some formatting
+                    Console.WriteLine("=====================");
+                    Console.WriteLine("= You have guessed: = ");
+                    foreach (string guess in guesses)
+                    {
+                        Console.WriteLine($"=\t{guess}\t    =");                                    //shows all the previous guesses
+                    }
+                    Console.WriteLine("=====================\n");
                     win = false;
                     guessCount += 1;
                     
