1e4c3b5933d8bd77534bed302489bd4eedcdf4e9
Tim Phillips <tim.phillips@covan.com>
2022-04-26 10:06:52 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Number_Guessing_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Random num = new Random();
+            int rndNum = num.Next(1, 11);
+            string yourGuess;
+            bool win = false;
+            string rndNumber = rndNum.ToString();
+            int guessCount = 1;
+
+            while (win == false)
+            {
+                Console.WriteLine("Guess the number between 1 and 10");
+                yourGuess = Console.ReadLine();
+
+                if (yourGuess == rndNumber)
+                {
+                    Console.WriteLine($"Congrats you guessed the number in {guessCount} guess(es)!");
+                    win = true;
+                    Console.ReadKey();
+                }
+
+                else
+                {
+                    Console.WriteLine("Sorry, try again. Make sure you enter a number.");
+                    win = false;
+                    guessCount += 1;
+                    
+                }
+            }
+            
+
+
+
+
+        }
+    }
+}
