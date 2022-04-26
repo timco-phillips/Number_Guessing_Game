237d7348a50f205929fe9f2337d9d678b898e121
Tim Phillips <tim.phillips@covan.com>
2022-04-26 10:22:01 -0500

    Changed number range from 1-20 to 1-25

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace Number_Guessing_Game
         {
             
             Random num = new Random();
-            int rndNum = num.Next(1, 21); //change number to 21 from 11
+            int rndNum = num.Next(1, 26); //change number to 26 from 11
             string yourGuess;
             bool win = false;
             string rndNumber = rndNum.ToString();
@@ -16,7 +16,7 @@ namespace Number_Guessing_Game
 
             while (win == false)
             {
-                Console.WriteLine("Guess the number between 1 and 20"); //changed string to reflect number change
+                Console.WriteLine("Guess the number between 1 and 25"); //changed string to reflect number change to 25
                 yourGuess = Console.ReadLine();
 
                 if (yourGuess == rndNumber)
