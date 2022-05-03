12cd911c0d1b86da3caea3049c57b7fff5fcee33
Aaron Jackson <aaron.jackson@covan.com>
2022-05-03 16:31:15 -0500

    Self

--- Program.cs
+++ Program.cs
@@ -13,13 +13,15 @@ namespace Number_Guessing_Game
             string yourGuess;
             bool win = false;
             string rndNumber = rndNum.ToString();
-            int guessCount = 1;
+            int guessCount = 0;
+            string temp;
             List<string> guesses = new List<string>(); //list to hold all of the guesses
 
             while (win == false)
             {
                 Console.Write("Guess a number between 1 and 30: "); //formatted the sentence a little differently
                 yourGuess = Console.ReadLine();
+                guessCount += 1;
                 guesses.Add(yourGuess);                                 //add guess to list
 
                 if (yourGuess == rndNumber)
@@ -29,19 +31,49 @@ namespace Number_Guessing_Game
                     Console.ReadKey();
                 }
 
+                else if ((int.Parse(yourGuess) + 3).ToString() == rndNumber || (int.Parse(yourGuess) - 3).ToString() == rndNumber
+                      || (int.Parse(yourGuess) + 2).ToString() == rndNumber || (int.Parse(yourGuess) - 2).ToString() == rndNumber
+                      || (int.Parse(yourGuess) + 1).ToString() == rndNumber || (int.Parse(yourGuess) - 1).ToString() == rndNumber)
+                      
+                {
+                    if ((int.Parse(yourGuess) + 1).ToString() == rndNumber || (int.Parse(yourGuess) - 1).ToString() == rndNumber)
+                    {
+                        temp = "Hot";
+                        Console.WriteLine($"\n{temp}\n");
+                        Console.WriteLine("=====================");
+                        Console.WriteLine("= You have guessed: = ");
+                        foreach (string guess in guesses)
+                        {
+                            Console.WriteLine($"=\t{guess}\t    =");                                    //shows all the previous guesses
+                        }
+                        Console.WriteLine("=====================\n");
+                    }
+                    else
+                    {
+                        temp = "warm";
+                        Console.WriteLine($"\n{temp}...\n");
+                        Console.WriteLine("=====================");
+                        Console.WriteLine("= You have guessed: = ");
+                        foreach (string guess in guesses)
+                        {
+                            Console.WriteLine($"=\t{guess}\t    =");                                    //shows all the previous guesses
+                        }
+                        Console.WriteLine("=====================\n");
+                    }
+                }
+
+                //else if ()
                 else
                 {
                     Console.WriteLine("\n -- Sorry, try again. Make sure you enter a number. --\n");   //Added some formatting
                     Console.WriteLine("=====================");
-                    Console.WriteLine("=Congrats! You have guessed: = "); //add congrats
+                    Console.WriteLine("= You have guessed: = ");
                     foreach (string guess in guesses)
                     {
                         Console.WriteLine($"=\t{guess}\t    =");                                    //shows all the previous guesses
                     }
                     Console.WriteLine("=====================\n");
                     win = false;
-                    guessCount += 1;
-                    
                 }
             }
             
